1939ef8015130c16d7eaf5d75c0bc2b57fbb14f3
Red the IT Guy <redtheitguy@gmail.com>
2020-07-20 13:02:43 +0100

    Added plugin base

---
+++ ForF/src/main/java/me/RedTheITGuy/ForF/Main.java
@@ -0,0 +1,10 @@
+package me.RedTheITGuy.ForF;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+	// Fired when the plugin is first enabled
+	@Override
+	public void onEnable() {
+		
+	}
+}
