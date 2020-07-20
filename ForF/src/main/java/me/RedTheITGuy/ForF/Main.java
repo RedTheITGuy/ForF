476c0e364e83bde63948622763a2bdda588f0f6b
Red the IT Guy <redtheitguy@gmail.com>
2020-07-20 14:02:47 +0100

    Added config loading

--- ForF/src/main/java/me/RedTheITGuy/ForF/Main.java
+++ ForF/src/main/java/me/RedTheITGuy/ForF/Main.java
@@ -5,6 +5,18 @@ public class Main extends JavaPlugin {
 	// Fired when the plugin is first enabled
 	@Override
 	public void onEnable() {
-		
+		// Creates the config if it does not exist
+		this.saveDefaultConfig();
+		// Gets all the info from the config
+		int minFirstPVP = this.getConfig().getInt("minFirstPVP");
+		int maxFirstPVP = this.getConfig().getInt("maxFirstPVP");
+		int minPVP = this.getConfig().getInt("minPVP");
+		int maxPVP = this.getConfig().getInt("maxPVP");
+		String pvpOnTitle = this.getConfig().getString("messages.enabled.title");
+		String pvpOnSub = this.getConfig().getString("messages.enabled.subtitle");
+		String pvpOnMessage = this.getConfig().getString("messages.enabled.message");
+		String pvpOffTitle = this.getConfig().getString("messages.disabled.title");
+		String pvpOffSub = this.getConfig().getString("messages.disabled.subtitle");
+		String pvpOffMessage = this.getConfig().getString("messages.disabled.message");
 	}
 }
